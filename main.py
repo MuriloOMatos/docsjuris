aed84716e3079a630850a9fcc53b00c5acc0cf6e
MuriloMatos <115664045+MuriloOMatos@users.noreply.github.com>
2025-04-08 09:34:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def calculos_emprestimo(form, num_emprestimos):
         vlr_total_emprestimo2 = Decimal(valor) * (1 + Decimal(taxa_contrato) / 100) ** Decimal(parcelas)
         org_bacen = (total_emprestimo - total_emprestimo_bacen)
         org_div = (total_emprestimo_geral / vlr_total_emprestimo1)
-        total_dobro += org_bacen * 2  
+        total_dobro = org_bacen * 2  
         valor_causa = Decimal(10000) + Decimal(total_dobro)
         comprometimento_renda = Decimal(parcela) + Decimal(parcela_pessoal)
         comprometimento_porcentagem = Decimal(comprometimento_renda) / Decimal(renda_mensal) * 100
