d099920a95fb20acba2af4be86696a3482a33242
MuriloMatos <115664045+MuriloOMatos@users.noreply.github.com>
2025-04-08 10:25:23 -0300

    dado-valor-causa

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def calculos_emprestimo(form, num_emprestimos):
             'org_div': f"{org_div:.2f}",
             'total_dobro': f"{total_dobro:.2f}",
             'valor_causa': f"{valor_causa:.2f}",
-            'dado_valor_causa': f"{dado_valor_causa:.f}",
+            'dado_valor_causa': f"{dado_valor_causa:2.f}",
             'comprometimento_renda': f"{comprometimento_renda:.2f}",
             'comprometimento_porcentagem': f"{comprometimento_porcentagem: .2f}",
             'renda_atual': f"{renda_atual:.2f}",
